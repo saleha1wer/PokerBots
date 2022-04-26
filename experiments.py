d96249a1a1a229577797874372ac32f80666d655
vikkyb <victorbatenburg@hotmail.com>
2022-04-26 11:06:32 +0200

    More TODOs

--- experiments.py
+++ experiments.py
@@ -10,18 +10,20 @@ Here we play many games (of n rounds each) using our bots and plot the results.
 TODO:
 - Make the 'play_game' function return the result of the game (the winner)
 - Plot results
+- Heuristic bot creates an error in line 93, chip_raise referenced before assignment
 """
+
 stakes = Stakes(0, (1, 2))
 starting_stacks = 200, 200, 200
 n_rounds = 50
 n_games = 100
 
-nls = NoLimitShortDeckHoldEm(stakes,starting_stacks)
+nls = NoLimitShortDeckHoldEm(stakes, starting_stacks)
 ra = RandomAgent(nls)
 ha = HeuristicAgent(nls)
 
 players = [ra, ha]
 
 for game in range(n_games):
-    play_game(n_rounds,players,nls,starting_stacks,stakes)
+    play_game(n_rounds, players, nls, starting_stacks, stakes)
 
