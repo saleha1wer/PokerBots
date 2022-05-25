4365bb51988d077aeb8c33c8578f0b8082934090
saleha1wer <s3305139@vuw.leidenuniv.nl>
2022-05-25 22:26:43 +0200

    state representation and network done

--- policy_network/network.py
+++ policy_network/network.py
@@ -7,21 +7,27 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
 class Network:
-    def __init__(self, input_shape,action_distribution_len):
-        self.in_shape = input_shape
+    def __init__(self, input_shapes,action_distribution_len):
+        self.in_shape_one = input_shapes[0]
+        self.in_shape_two = input_shapes[1]
+
         self.out_shape = action_distribution_len
         self.network = None
 
     def initialize_network(self):
 
-        inp = Input(shape=self.in_shape) # (num_features,) 
+        inp_one = Input(shape=self.in_shape_one) # (num_features,) 
+        inp_two = Input(shape=self.in_shape_two) # (num_features,) 
 
-        full = Dense(256, activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01))(inp)
+        full = Dense(32, activation='relu',kernel_regularizer=tf.keras.regularizers.l2(0.01))(inp_one)
         batch = BatchNormalization()(full)
-        full_1 = Dense(128, activation='relu')(batch)
-        full_2 = Dense(64)(full_1)
-        out = Dense(self.out_shape, activation='softmax',name='action_dis')(full_2)
-        network = Model(inputs=[inp], outputs =[out], name='Features2WinRate&ActDis')
+        full_1 = Dense(20, activation='relu')(batch)
+        full_2 = Dense(64, activation='relu')(inp_two)
+        full_3 = Dense(40, activation='relu')(full_2)
+        conc = Concatenate()([full_3,full_1])
+        full_4 = Dense(20)(conc)
+        out = Dense(self.out_shape, activation='softmax',name='action_dis')(full_4)
+        network = Model(inputs=[inp_one,inp_two], outputs =out, name='Features2ActDis')
         network.compile(
             loss = ['categorical_crossentropy'],
             optimizer = tf.keras.optimizers.Adam(),
@@ -29,31 +35,27 @@ class Network:
         self.network = network
         print(network.summary())
 
-    def _read_data(self, game_array,action_ds):
+    def _read_data(self,game_info,opponent_info,action_ds):
         # game_array = ... # Array of shape (N, num_feature_layers, height, width) 
         # action_ds = ...  # Array of size (N,1)
 
 
-        print('shape of input data: ',game_array.shape)
+        print('shape of input data - 1: ',game_info.shape)
+        print('shape of input data - 2: ',opponent_info.shape)
         print('shape of output data: ',action_ds.shape)
+        X_train_gi,X_test_gi,X_train_oi,X_test_oi, y_train, y_test = train_test_split(game_info,opponent_info,action_ds, test_size=0.05) 
 
+        print('Train: {}'.format(X_train_gi.shape, y_train.shape))
+        print('Valid: {}'.format(X_test_gi.shape, y_test.shape))
 
-        X_train,X_test, y_train, y_test = train_test_split(game_array,action_ds, test_size=0.05) 
-
-
-
-        print('Train: Shape of X:{}, Shape of y: {}'.format(X_train.shape, y_train.shape))
-        print('Test: Shape of X:{}, Shape of y: {}'.format(X_test.shape, y_test.shape))
-
-
-        return X_train,X_test, y_train, y_test
+        return X_train_gi,X_test_gi,X_train_oi,X_test_oi, y_train, y_test
         
-    def train_network(self, X,y,epochs):
-        X_train,X_test, y_train, y_test = self._read_data(X,y)
+    def train_network(self, game_array,opponent_array,y,epochs):
+        X_train_game,X_test_game,X_train_opponents,X_test_opponents, y_train, y_test = self._read_data(game_array,opponent_array,y)
         self.network.fit(
-            x = X_train,
+            x = [X_train_game,X_train_opponents],
             y = y_train,
-            validation_data = ([X_test], [y_test]),
+            validation_data = ([X_test_game,X_test_opponents], y_test),
             epochs = epochs,
             batch_size=16
         )
